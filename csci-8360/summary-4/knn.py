7790f0842b9ff17d1258e0091c0443c0e792e793
Patrick Davlin <patrick@davlin.io>
2021-11-05 16:05:15 -0500

    Update knn.py

--- csci-8360/summary-4/knn.py
+++ csci-8360/summary-4/knn.py
@@ -12,15 +12,18 @@ vectorizer = TfidfVectorizer()
 X = vectorizer.fit_transform(newsgroups_all.data)
 y = newsgroups_all.target
 
-for k_value in range(1, 21):
-    sum = 0
-    for _ in range(0,10):
-        classifier = KNeighborsClassifier(n_neighbors=k_value)
+for test_size in [0.6, 0.4, 0.2, 0.1]:
+    print('results for k-Values in test size {}'.format(test_size))
+    for k_value in range(1, 21):
+        sum = 0
+        for _ in range(0, 10):
+            classifier = KNeighborsClassifier(n_neighbors=k_value)
 
-        newsgroups_train, newsgroups_test, y_train, y_test = train_test_split(X, y, test_size=0.6)
-        classifier.fit(newsgroups_train, y_train)
+            newsgroups_train, newsgroups_test, y_train, y_test = train_test_split(
+                X, y, test_size=test_size)
+            classifier.fit(newsgroups_train, y_train)
 
-        prediction = classifier.predict(newsgroups_test)
+            prediction = classifier.predict(newsgroups_test)
 
-        sum = sum + accuracy_score(y_test, prediction)
-    print('{},{}'.format(k_value, sum/10))
+            sum = sum + accuracy_score(y_test, prediction)
+        print('{}, {}'.format(k_value, sum/10))
